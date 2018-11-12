025c7ffe760c3914df5375afeef9216b996874c2
elijah <elijaveloper@gmail.com>
2018-11-12 09:46:13 +0700

    Just an update

--- index.php
+++ index.php
@@ -18,12 +18,20 @@
         font-family: 'Concert One';
         font-size: 4em;
       }
-      input{
+      #title{
+        font-family: 'Concert One';
+        font-size: 4em;
+        text-align: center;
+        margin: 40px;
+        color:#4285F4;
+      }
+      input,textarea{
         font-family: 'Concert One';
         font-size: 1em;
         border-radius: 15px;
         border: 3px solid #ededed;
         padding: 10px;
+        cursor: url("img/hand.png"), auto;
       }
       input[type=text]:focus, textarea:focus {
         outline:none;
@@ -41,7 +49,7 @@
         background-color: #4285F4;
         color: #FFFFFF;
         padding: 25px;
-        cursor: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/9632/happy.png"), auto;
+        cursor: url("img/hand.png"), auto;
         box-shadow: 0px 9px 0px #2142a2, 0px 9px 25px rgba(0,0,0,.7);
       }
 
@@ -53,20 +61,29 @@
     </style>
   </head>
   <body>
+    <div class="h1" id="title">
+      Registration!
+    </div>
     <div class="container">
     </div>
 
   </body>
   <script type="text/javascript">
-    console.log("here");
     var Container = document.querySelector(".container");
-    var Questions = "Name:30,Nickname:30,Age:4,Level:8,Section:4,Student Number:4,Gender:4,School:30,Favourite Subject:30,Favourite Food:30,Favourite Teacher:30".split(",");
+    var Questions = "Name:30,Nickname:30,Age:4,Level:10,Section:4,Student Number:4,Gender:7,School:30,Favourite Subject:30,Favourite Food:30,Favourite Teacher:30".split(",");
+    var Answers = "{";
     var Colors = "#4285F4,#DB4437,#F4B400,#0F9D58".split(",");
     Questions.forEach((question,i) => {
       question = question.split(":");
-      Container.innerHTML += "<div class='section'><div class='row'>"+question[0]+"</div>" +
-      "<div class='row'><input type='text' name='"+question[0]+"' size="+question[1]+"/></div></div>";
+      if(question[0] == "School"){
+        Container.innerHTML += "<div class='section'><div class='row'>"+question[0]+"</div>" +
+          "<div class='row'><textarea class='questions' id='"+question[0].split(" ").join("")+"' name='"+question[0]+"' cols='"+question[1]+" rows=2'></textarea></div></div>";
+      }else{
+        Container.innerHTML += "<div class='section'><div class='row'>"+question[0]+"</div>" +
+        "<div class='row'><input class='questions' id='"+question[0].split(" ").join("")+"' type='text' name='"+question[0]+"' size='"+question[1]+"'/></div></div>";
+      }
     });
+    var school = document.getElementById("School");
     Container.innerHTML += "<div class='section'><div class='row'><input type='button' id='submit' value='submit'></div></div>"
     var itr = 0;
     var Sections = document.getElementsByClassName("section");
@@ -74,5 +91,20 @@
       Sections[i].style.color = Colors[itr];
       itr = itr < 3 ? itr + 1 : 0;
     }
+
+    var Responses = document.getElementsByClassName("questions");
+    var submitButton = document.getElementById("submit");
+    submitButton.onclick = ()=>{
+      for(var i=0; i < Responses.length; i++){
+        var value = Responses[i].value;
+        var id = Responses[i].id;
+        Answers += "\""+id+"\":\""+value+"\"";
+        Answers += (i == Responses.length - 1) ? "" : ",";
+      }
+      Answers += "}";
+      var AnswersJSON = JSON.parse(Answers);
+      console.log(AnswersJSON);
+    };
+
   </script>
 </html>
